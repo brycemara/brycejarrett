a9aeafacd14d6bec238d6ea9e0f63ad11b2b7fb5
Bryce Mara Jarrett <brycej98@gmail.com>
2024-04-30 19:41:09 -0600

    fix: add build

---
+++ next.config.js
@@ -0,0 +1,12 @@
+/**
+ * @type {import('next').NextConfig}
+ */
+const nextConfig = {
+    output: 'export',
+    distDir: 'docs',
+    images: {
+        unoptimized: true
+    }
+  }
+   
+  module.exports = nextConfig
