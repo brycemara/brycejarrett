98c1d6cd8ce5056013733a1fbaca71f15939f8f7
Bryce Mara Jarrett <brycej98@gmail.com>
2024-05-15 14:38:01 -0400

    fix: update landing text

--- src/components/Landing.tsx
+++ src/components/Landing.tsx
@@ -5,10 +5,13 @@ import { me } from "@/public/constants";
 const Landing = () => {
   return (
     <div className="text-white p-8 flex sm: flex-col lg:flex-row justify-evenly items-center">
-      <div className="flex flex-col justify-center items-center p-4">
-        <p>Hello !</p>
-        {/* TODO: format the json object better */}
-        <p>const me = {JSON.stringify(me)}</p>
+      <div className="flex flex-col w-1/3 justify-center items-center p-4">
+        <p>
+          I'm Bryce Jarrett, a Software Engineer based in Brooklyn NY. I've been
+          coding in the real world for about 3 years now. I am passionate about
+          solving problems, leaving things better than I found them, and growing
+          from every experience.
+        </p>
       </div>
       <Image src={headshot} width={600} height={600} alt="headshot" />
     </div>
